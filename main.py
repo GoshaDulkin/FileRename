11af7f05329cba93e2ca64ef3dd341cfbc6431ee
gdulkin <gosha.dulkin@gmail.com>
2022-02-22 16:19:13 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+os.chdir('C:/Users/gdulkin/Documents/MAPScript')
+
+for f in os.listdir():
+    file_name, file_ext = os.path.splitext(f)
+
+    if file_name.count('-') == 4:
+        f_map, f_dep, f_ref, f_part, f_num = file_name.split('-')
+        new_name = '{}-{}-{}-{}-{}{}'.format(f_part, f_num, f_map, f_dep, f_ref, file_ext)
+
+        os.rename(f, new_name)
+
+    if file_name.count('-') == 5:
+        f_map, f_dep, f_ref, f_part, f_part2, f_num = file_name.split('-')
+        new_name = '{}-{}-{}-{}-{}-{}{}'.format(f_part, f_part2, f_num, f_map, f_dep, f_ref, file_ext)
+
+        os.rename(f, new_name)
+
+    if file_name.count('-') == 6:
+        f_map, f_dep, f_ref, f_part, f_part2, f_part3, f_num = file_name.split('-')
+        new_name = '{}-{}-{}-{}-{}-{}-{}{}'.format(f_part, f_part2, f_part3, f_num, f_map, f_dep, f_ref, file_ext)
+
+        os.rename(f, new_name)
